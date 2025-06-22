4ba2b6fff8b8a4ef35446019446e8c31558dc01e
Lucas Sanches <sancheseducacional@gmail.com>
2025-06-22 13:13:38 -0300

    adicionando e organizando a pasta de queries

---
+++ Queries/queries-basicas.sql
@@ -0,0 +1,12 @@
+-- Estrutura básica de uma query
+
+SELECT 
+    *
+FROM tb_customers
+
+-- Selecionando dados de tabelas específicas
+
+SELECT 
+    customer_id,
+    customer_state
+FROM tb_customers
